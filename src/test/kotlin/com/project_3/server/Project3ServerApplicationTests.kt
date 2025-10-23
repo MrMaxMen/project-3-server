5d5ad3953c71b34bd655641a0b844402f6481ed3
Maxim <maxmen14521@gmail.com>
2025-10-23 14:07:55 +0100

    Initial commit

---
+++ src/test/kotlin/com/project_3/server/Project3ServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.project_3.server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Project3ServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
